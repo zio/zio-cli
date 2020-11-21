ee21e7514824f69b8f77d61cf83346603ba37ffa
Porcupine <luki.pol@gmail.com>
2020-11-21 15:55:20 +0100

    feat: download jar from url

--- installer.sh
+++ installer.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-target="./local/bin"
+# TODO: read from command line
+cliDownloadUrl="https://github.com/JakDar/zio-cli-test/releases/download/0.0.1/zio-cli-test-0.0.1.jar"
+cliFile=$(basename "$cliDownloadUrl")
 graalvmVersion="20.3.0"
 javaVersion="java8"
+# TODO: allow to customize
 downloadDir="/tmp/test"
-cliUrl=""
+installLocation="./local/bin"
 
-jarPath="/home/porcupine/projects/zio-cli-test/target/scala-2.13/zio-awesome-project-assembly-0.0.1.jar"
 
 case $(uname -s) in
   Darwin*) os="mac" ;;
@@ -19,6 +21,7 @@ esac
 
 graalvmPath="${downloadDir}/graalvm-ce-${javaVersion}-${graalvmVersion}"
 
+# TODO: check if graalvm exists locally
 installGraalVm() {
     command -v curl > /dev/null || fail curl
     printf "Downloading GraalVM\n"
@@ -31,27 +34,29 @@ installGraalVm() {
 }
 
 installNativeImage() {
+    PATH="${PATH}:${graalvmPath}/bin"
     gu install native-image
+}
+
+downloadCli() {
+    downloadFile="${downloadDir}/${cliFile}"
+    [ -f "${downloadFile}" ] || curl -Lso "${downloadFile}" "$cliDownloadUrl"
 }
 
 buildNativeImage() {
     PATH="${PATH}:${graalvmPath}/bin"
     GRAALVM_HOME="${graalvmPath}"
 
-    native-image -jar "$jarPath"
+    # TODO: do not rebuild every time
+    # TODO: set the install location
+    native-image -jar "${downloadDir}/${cliFile}" application
 }
-
-downloadCli() {
-    file=$(basename "$cliUrl")
-    downloadFile="${downloadDir}/${file}"
-    [ -f "${downloadFile}" ] || curl -Lso "${downloadFile}" "$cliUrl"
-}
-
 
 fail() {
   printf "fail\n\033[1m\033[41m FAIL \033[0m \033[1m"
   case "$1" in
     curl)     printf "Could not find \033[0;36mcurl\033[0m, which is required by the install script." ;;
+    os)       printf "Could not identify the operating system type" ;;
   esac
   printf "\033[0m\n"
   exit 1
