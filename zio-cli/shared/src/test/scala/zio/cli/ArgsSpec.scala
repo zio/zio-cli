e59a2a3991ac539c9d82b210f2d57e618806701b
Mohamedali10 <bouine.mohamedali@gmail.com>
2020-11-22 12:09:52 +0100

    add spec for file arguments and fix the method repeat well formated file.

--- zio-cli/shared/src/test/scala/zio/cli/ArgsSpec.scala
+++ zio-cli/shared/src/test/scala/zio/cli/ArgsSpec.scala
@@ -1,56 +1,56 @@
 package zio.cli
 
-import java.io.{File => JFile}
-import java.nio.file.{Path => JPath, Paths}
+import java.io._
+import java.nio.file.Paths
 
 import zio.cli.HelpDoc.Paragraph
 import zio.cli.HelpDoc.Span.Text
-import zio.test.{suite, _}
 import zio.test.Assertion._
+import zio.test._
 
 object ArgsSpec extends DefaultRunnableSpec {
-
-  val argsFile: JFile     = new JFile("zio-cli/shared/src/test/scala/zio/cli","ArgsFileExample")
-  val argsFilePath: JPath = Paths.get(argsFile.getAbsolutePath)
-
   def spec = suite("ArgsSpec")(
-    suite("File Arguments Spec")(
-      testM("Existing file") {
-        val arg = Args.file("files", Exists.Yes).repeat
-        assertM(
-          arg.validate(argsFile.getAbsolutePath :: Nil, ParserOptions.default)
-        ) (equalTo(List.empty[String] -> List(argsFilePath)))
-      },
-      testM("Not Found file") {
-        val arg = Args.file("files", Exists.Yes).repeat
-        assertM(
-          arg.validate("notFound.file" :: Nil, ParserOptions.default).either
-          ) (isLeft(equalTo(Paragraph(Text("Path 'notFound.file' must exist.")))))
-      },
-      testM("Non Existing file") {
-        val arg = Args.file(Exists.No).repeat
-        assertM  (
-          arg.validate("doesNotExist.file" :: Nil, ParserOptions.default).either
-        ) (isRight)
-      },
-      testM("Path of non regular file") {
-        val arg = Args.file.repeat
-        assertM  (
-          arg.validate( "notRegular.file" :: Nil, ParserOptions.default).either
-        ) (isLeft(equalTo(Paragraph(Text("Expected path 'notRegular.file' to be a regular file.")))))
-      },
-      testM("Combination of existing and non existing files") {
-        val arg = Args.file.repeat
-        assertM(
-          arg.validate(List(argsFile.getAbsolutePath, "nonExisting.file"), ParserOptions.default).either
-        ) (isLeft(equalTo(Paragraph(Text("Expected path 'nonExisting.file' to be a regular file.")))))
-      },
-      testM("Combination of existing files") {
-        val arg = Args.file.repeat
-        assertM(
-          arg.validate(List(argsFile.getAbsolutePath, argsFile.getAbsolutePath), ParserOptions.default)
-        ) (equalTo(List.empty[String] -> List(argsFilePath, argsFilePath)))
-      }
-    )
+    testM("Existing file") {
+      val arg = Args.file("files", Exists.Yes).repeat
+
+      assertM(
+        arg.validate(argsFile.getAbsolutePath :: Nil, ParserOptions.default)
+      )(equalTo(List.empty[String] -> List(argsFilePath)))
+    },
+    testM("Not Found file") {
+      val arg = Args.file("files", Exists.Yes).repeat
+      assertM(
+        arg.validate("notFound.file" :: Nil, ParserOptions.default).either
+      )(isLeft(equalTo(Paragraph(Text("Path 'notFound.file' must exist.")))))
+    },
+    testM("Non Existing file") {
+      val arg = Args.file(Exists.No).repeat
+
+      assertM(
+        arg.validate("doesNotExist.file" :: Nil, ParserOptions.default).either
+      )(isRight)
+    },
+    testM("Path of non regular file") {
+      val arg = Args.file.repeat
+      assertM(
+        arg.validate("notRegular.file" :: Nil, ParserOptions.default).either
+      )(isLeft(equalTo(Paragraph(Text("Expected path 'notRegular.file' to be a regular file.")))))
+    },
+    testM("Combination of existing and non existing files") {
+      val arg = Args.file.repeat
+      assertM(
+        arg.validate(List(argsFile.getAbsolutePath, "nonExisting.file"), ParserOptions.default).either
+      )(isLeft(equalTo(Paragraph(Text("Expected path 'nonExisting.file' to be a regular file.")))))
+    },
+    testM("Combination of existing files") {
+      val arg = Args.file.repeat
+      assertM(
+        arg.validate(List(argsFile.getAbsolutePath, argsFile.getAbsolutePath), ParserOptions.default)
+      )(equalTo(List.empty[String] -> List(argsFilePath, argsFilePath)))
+    }
   )
+
+  val argsFile     = new File("zio-cli/shared/src/test/scala/zio/cli/ArgsFileExample")
+  val argsFilePath = Paths.get(argsFile.getAbsolutePath)
+
 }
