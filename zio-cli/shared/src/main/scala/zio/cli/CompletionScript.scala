8dccc2a66415c5a90daeca55e7ae22562c8b7ab8
William Harvey <william.harvey@ziverge.com>
2021-10-30 12:48:17 -0600

    Add a generator for Bash tab completion scripts.

---
+++ zio-cli/shared/src/main/scala/zio/cli/CompletionScript.scala
@@ -0,0 +1,34 @@
+package zio.cli
+import zio.cli.ShellType.Bash
+import zio.cli.ShellType.ZShell
+import java.nio.file.{Path => JPath}
+
+object CompletionScript {
+  def apply(pathToExecutable: JPath, programNames: Set[String], shellType: ShellType): String = shellType match {
+    case Bash => bash(pathToExecutable, programNames)
+    case ZShell => ???
+  }
+  private[this] def bash(pathToExecutable: JPath, programNames: Set[String]): String = 
+    s"""|#/usr/bin/env bash
+        |_${programNames.head}()
+        |{
+        |  local CMDLINE
+        |  local IFS=$$'\\n'
+        |  CMDLINE=(--shell-type bash --shell-completion-index $$COMP_CWORD)
+        |  
+        |  INDEX=0
+        |  for arg in $${COMP_WORDS[@]}; do
+        |    export COMP_WORD_$$INDEX=$${arg}
+        |    (( INDEX++ ))
+        |  done
+        |
+        |  COMPREPLY=( $$($pathToExecutable \"$${CMDLINE[@]}\") )
+        |
+        |  # Unset the environment variables.
+        |  unset $$(compgen -v | grep "^COMP_WORD_")
+        |}
+        |
+        |""".stripMargin + programNames.map(programName =>
+          s"complete -F _${programNames.head} $programName"
+        ).mkString(System.lineSeparator)
+}
