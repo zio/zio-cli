35e6bf5e345aae08bce4e8c443a3e3df0501b30a
William Harvey <william.harvey@ziverge.com>
2021-10-31 00:11:26 -0600

    Add documentation for Bash/Zsh tab completion.

--- zio-cli/shared/src/main/scala/zio/cli/Completion.scala
+++ zio-cli/shared/src/main/scala/zio/cli/Completion.scala
@@ -3,7 +3,7 @@ package zio.cli
 object Completion {
   def complete(shellType: ShellType, words: List[String], index: Int): List[String] = {
     // TODO: The dummy completions below are just a proof of concept. They
-    // should be replated with legitimate completions.
+    // should be replaced with legitimate completions.
     val dummyCompletions = (
       words.zipWithIndex.map{case (word, i) => s"$word-$i"} ++
       List(s"cursor-index-$index") ++
