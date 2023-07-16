3d2ac542ad6829c08da54da2d4fe374b70e7fae4
Terry L <8069171+ithinkicancode@users.noreply.github.com>
2023-07-16 11:23:16 -0400

    Fixed issue-226 (exception when parsing non-key-value-pair) (#229)
    * Fixed issue-226 (exception when parsing non-key-value-pair)
    
    * Fixed formatting

--- zio-cli/shared/src/test/scala/zio/cli/OptionsSpec.scala
+++ zio-cli/shared/src/test/scala/zio/cli/OptionsSpec.scala
@@ -351,6 +351,44 @@ object OptionsSpec extends ZIOSpecDefault {
         val r = m.validate(List("--defs", "key1=v1", "key2=v2", "--verbose"), CliConfig.default)
 
         assertZIO(r)(equalTo(List("--verbose") -> Map("key1" -> "v1", "key2" -> "v2")))
+      },
+      test(
+        "validate should keep non-key-value parameters that follow the key-value pairs (each preceded by alias -d)"
+      ) {
+        val r = m.validate(
+          List("-d", "key1=val1", "-d", "key2=val2", "-d", "key3=val3", "arg1", "arg2", "--verbose"),
+          CliConfig.default
+        )
+
+        assertZIO(r)(
+          equalTo(List("arg1", "arg2", "--verbose") -> Map("key1" -> "val1", "key2" -> "val2", "key3" -> "val3"))
+        )
+      },
+      test(
+        "validate should keep non-key-value parameters that follow the key-value pairs (only the first key/value pair is preceded by alias)"
+      ) {
+        val r = m.validate(
+          List("-d", "key1=val1", "key2=val2", "key3=val3", "arg1", "arg2", "--verbose"),
+          CliConfig.default
+        )
+
+        assertZIO(r)(
+          equalTo(List("arg1", "arg2", "--verbose") -> Map("key1" -> "val1", "key2" -> "val2", "key3" -> "val3"))
+        )
+      },
+      test(
+        "validate should keep non-key-value parameters that follow the key-value pairs (with a 'mixed' style of proceeding -- name or alias)"
+      ) {
+        val r = m.validate(
+          List("-d", "key1=val1", "key2=val2", "--defs", "key3=val3", "key4=", "arg1", "arg2", "--verbose"),
+          CliConfig.default
+        )
+
+        assertZIO(r)(
+          equalTo(
+            List("key4=", "arg1", "arg2", "--verbose") -> Map("key1" -> "val1", "key2" -> "val2", "key3" -> "val3")
+          )
+        )
       }
     )
   )
