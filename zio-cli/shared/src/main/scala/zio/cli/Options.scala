9ca731d64917b6bcc6bf661393cd597a7a24d4a4
pfeme <pablofeme@gmail.com>
2023-07-19 21:13:46 +0200

    fmt

--- zio-cli/shared/src/main/scala/zio/cli/Options.scala
+++ zio-cli/shared/src/main/scala/zio/cli/Options.scala
@@ -271,41 +271,43 @@ object Options extends OptionsPlatformSpecific {
     def validate(args: List[String], conf: CliConfig): IO[ValidationError, (List[String], A)] =
       args match {
         case head :: tail =>
-          if (head.trim.matches("^-{1}([^-]{2,}$)")) 
-            validate(head.drop(1).map(c => s"-$c") :: tail, conf).catchSome {
-              case _: ValidationError => validate(tail, conf).map {
-                case (rest, a) => (head :: rest, a)
+          if (head.trim.matches("^-{1}([^-]{2,}$)"))
+            validate(head.drop(1).map(c => s"-$c") :: tail, conf).catchSome { case _: ValidationError =>
+              validate(tail, conf).map { case (rest, a) =>
+                (head :: rest, a)
               }
             }
           else {
             val (rest, supported) = processArg(head, tail, conf)
             if (supported) {
-            primType match {
-              case _: PrimType.Bool =>
-                primType
-                  .validate(None, conf)
-                  .mapBoth(e => ValidationError(ValidationErrorType.InvalidValue, p(e)), rest -> _)
-              case _ =>
-                primType
-                  .validate(rest.headOption, conf)
-                  .mapBoth(e => ValidationError(ValidationErrorType.InvalidValue, p(e)), rest.drop(1) -> _)
-            }
-          } else {
-            validate(rest, conf).map { case (args, a) =>
-              (head :: args, a)
-            }.catchSome {
-              case e: ValidationError => if (
-                  name.length > conf.autoCorrectLimit + 1 && 
+              primType match {
+                case _: PrimType.Bool =>
+                  primType
+                    .validate(None, conf)
+                    .mapBoth(e => ValidationError(ValidationErrorType.InvalidValue, p(e)), rest -> _)
+                case _ =>
+                  primType
+                    .validate(rest.headOption, conf)
+                    .mapBoth(e => ValidationError(ValidationErrorType.InvalidValue, p(e)), rest.drop(1) -> _)
+              }
+            } else {
+              validate(rest, conf).map { case (args, a) =>
+                (head :: args, a)
+              }.catchSome { case e: ValidationError =>
+                if (
+                  name.length > conf.autoCorrectLimit + 1 &&
                   AutoCorrect.levensteinDistance(head, fullName, conf) <= conf.autoCorrectLimit
-                ) ZIO.fail(
-                ValidationError(
-                  ValidationErrorType.InvalidValue,
-                  p(error(s"""The flag "$head" is not recognized. Did you mean ${fullName}?"""))
                 )
-              ) else ZIO.fail(e)
+                  ZIO.fail(
+                    ValidationError(
+                      ValidationErrorType.InvalidValue,
+                      p(error(s"""The flag "$head" is not recognized. Did you mean ${fullName}?"""))
+                    )
+                  )
+                else ZIO.fail(e)
+              }
             }
           }
-        }
         case Nil =>
           ZIO.fail(
             ValidationError(ValidationErrorType.MissingValue, p(error(s"Expected to find ${fullName} option.")))
