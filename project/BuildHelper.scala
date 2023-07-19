65dcb3e8cdc7a11652c59f802686c9d631d34420
Terry Lin <tlin.gta@gmail.com>
2023-07-19 11:12:44 -0400

    Upgraded scalafix to upgrade semanticdb

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -12,7 +12,7 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 object BuildHelper {
 
   val Scala212 = "2.12.18"
-  val Scala213 = "2.13.10"
+  val Scala213 = "2.13.11"
   val Scala3   = "3.3.0"
 
   val SilencerVersion = "1.17.13"
