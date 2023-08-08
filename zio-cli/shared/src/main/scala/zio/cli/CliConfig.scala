eaa673c64b05ef3509ea7bf2bc1d69e7ab8de0f8
pfeme <pablofeme@gmail.com>
2023-08-08 11:16:31 +0200

    configurable synopsis

--- zio-cli/shared/src/main/scala/zio/cli/CliConfig.scala
+++ zio-cli/shared/src/main/scala/zio/cli/CliConfig.scala
@@ -8,12 +8,20 @@ import zio.{URIO, ZIO}
  * @param caseSensitive
  *   Whether or not to be case sensitive.
  * @param autoCorrectLimit
- *   Threshold for when to show auto correct suggestions
+ *   Threshold for when to show auto correct suggestions.
+ * @param finalCheckBuiltIn
+ *   Whether or not to check for a BuiltIn option even if it is not a valid command.
+ * @param showAllNames
+ *   Whether or not to show all the names of an option in the synopsis of a command.
+ * @param showTypes
+ *   Whether or not to show the type of an option in the synopsis of a command.
  */
 final case class CliConfig(
   caseSensitive: Boolean,
   autoCorrectLimit: Int,
-  finalCheckBuiltIn: Boolean = true
+  finalCheckBuiltIn: Boolean = true,
+  showAllNames: Boolean = true,
+  showTypes: Boolean = true
 ) {
   def normalizeCase(text: String): String = if (caseSensitive) text else text.toLowerCase()
 
