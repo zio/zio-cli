a435e1becee42dbd5953d00e259ef41de0892677
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2025-03-02 13:08:44 -0800

    fix: inferred `Any`

--- zio-cli/shared/src/main/scala/zio/cli/ZIOCli.scala
+++ zio-cli/shared/src/main/scala/zio/cli/ZIOCli.scala
@@ -9,7 +9,7 @@ trait ZIOCli extends ZIOApp {
   override def run =
     for {
       args <- ZIOAppArgs.getArgs
-      result <- cliApp.run(args.toList).catchSome { case CliError.Parsing(_) =>
+      result <- cliApp.run(args.toList).catchSome[Environment with ZIOAppArgs with Scope, CliError[Any], Any] { case CliError.Parsing(_) =>
                   // Validation errors are pretty printed by clipApp.run
                   exit(ExitCode.failure)
                 }
