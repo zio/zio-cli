41abdbee09c209a27b262f86afa4389689e376bb
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2025-03-02 13:09:12 -0800

    format

--- zio-cli/shared/src/main/scala/zio/cli/ZIOCli.scala
+++ zio-cli/shared/src/main/scala/zio/cli/ZIOCli.scala
@@ -9,9 +9,10 @@ trait ZIOCli extends ZIOApp {
   override def run =
     for {
       args <- ZIOAppArgs.getArgs
-      result <- cliApp.run(args.toList).catchSome[Environment with ZIOAppArgs with Scope, CliError[Any], Any] { case CliError.Parsing(_) =>
-                  // Validation errors are pretty printed by clipApp.run
-                  exit(ExitCode.failure)
+      result <- cliApp.run(args.toList).catchSome[Environment with ZIOAppArgs with Scope, CliError[Any], Any] {
+                  case CliError.Parsing(_) =>
+                    // Validation errors are pretty printed by clipApp.run
+                    exit(ExitCode.failure)
                 }
     } yield result
 }
