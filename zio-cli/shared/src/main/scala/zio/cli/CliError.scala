72af712c0c8d4bce1b4812cf85f23f52c9d421c3
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2025-08-26 12:49:27 -0700

    fix rendering of CliError message/cause

--- zio-cli/shared/src/main/scala/zio/cli/CliError.scala
+++ zio-cli/shared/src/main/scala/zio/cli/CliError.scala
@@ -1,8 +1,23 @@
 package zio.cli
 
 import java.io.IOException
+import zio.cli.CliError.BuiltIn
+import zio.cli.CliError.IO
+import zio.cli.CliError.Parsing
+import zio.cli.CliError.Execution
 
-sealed trait CliError[+E] extends Exception
+sealed trait CliError[+E] extends Exception { self =>
+  override final def getMessage(): String =
+    "zio-cli app failed"
+  override final def getCause(): Throwable =
+    self match {
+      case BuiltIn(e)              => e
+      case IO(e)                   => e
+      case Parsing(e)              => e
+      case Execution(e: Throwable) => e
+      case Execution(_)            => null
+    }
+}
 
 object CliError {
 
